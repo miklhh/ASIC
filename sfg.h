0d3f1e46fc3ec757c0fd39f26051ea3e9cdcc071
Mikael Henriksson <mike.zx@hotmail.com>
2022-07-15 09:25:46 +0200

    Start work on SFGs

--- sfg.h
+++ sfg.h
@@ -2,19 +2,59 @@
 #define _SFG_H
 
 #include "graph.h"
-#include "op.h"
 
 #include <vector>
 
-struct SFGNode {
-    Op type{};
-    std::string name{};
-    std::vector<NodeID> inputs{};
-    std::vector<NodeID> outputs{};
+/*
+ * Each Arc in an SFG is associated with an operand.
+ */
+enum class OperandType {
+
+    // Delay operand type
+    DELAY,
+
+    // Addition operand types: SUM = TERM + TERM 
+    TERM,
+
+    // Subtraction operand types: ??? = 
+
+    // Multiplication operand types: PRODUCT = MULTIPLIER * MULTIPLICAND
+    MULTIPLIER,
+    MULTIPLICAND,
+
+    // Division operand types: QUOTIENT = DIVISOR / DIVIDEND ???
+    DIVISOR,
+    DIVIDEND,
 };
 
-class SFG : public Graph<SFGNode> {
-    
+struct SFGArc : public BaseArc {
+    SFGArc(NodeID src, NodeID dst, OperandType op) : BaseArc(src, dst), op{op} {}
+    OperandType op;
+};
+
+struct SFGNode : public BaseNode<> {
+    std::string name{};
+};
+
+class SFG : public Graph<SFGNode, SFGArc> {
+public:
+    // Add a delay element to the SFG
+    void add_delay(NodeID in, NodeID out) {
+        create_arc( SFGArc(in, out, OperandType::DELAY) );
+    }
+
+    // Add an adder to the SFG.
+    void add_adder(NodeID in1, NodeID in2, NodeID res) {
+        create_arc( SFGArc(in1, res, OperandType::TERM) );
+        create_arc( SFGArc(in2, res, OperandType::TERM) );
+    }
+
+    // Add an multiplier to the SFG.
+    void add_multiplier(NodeID in1, NodeID in2, NodeID res) {
+        create_arc( SFGArc(in1, res, OperandType::MULTIPLIER) );
+        create_arc( SFGArc(in2, res, OperandType::MULTIPLICAND) );
+    }
+
 };
 
 #endif
--- tests/test_sfg.cc
+++ tests/test_sfg.cc
@@ -6,6 +6,10 @@
 TEST_CASE("3 tap FIR filter")
 {
     SFG fir{};
-    NodeID input = fir.create_node();
+    NodeID input1 = fir.create_node();
+    NodeID input2 = fir.create_node();
+    NodeID output = fir.create_node();
+    fir.add_adder(input1, input2, output);
+
 }
 
